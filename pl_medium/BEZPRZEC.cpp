77f3114b3bf982dbd6794be3f884cd658a4c31a5
WN <roonie9@wp.pl>
2018-06-08 13:17:40 +0200

    Solved BEZPRZEC (c++)

---
+++ pl_medium/BEZPRZEC.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    if (int A = 1) {
+        if (int B = 2) {
+            if (scanf("%d%d", &A, &B)) {}
+            if (printf("%d", A + B)) {}
+        }
+    }
+}
